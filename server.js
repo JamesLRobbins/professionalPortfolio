029d7e2a3e295285a02a995d79832259cc15632f
James Robbins <jameslrobbins86@gmail.com>
2020-01-28 17:53:59 -0600

    Server Fix

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.post('/api/form', (req, res) => {
     nodemailer.createTestAccount((err, account) => {
