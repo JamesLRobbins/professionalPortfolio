55a3b527c09b3c18f5323f9f07671e44c1e3854d
James Robbins <jameslrobbins86@gmail.com>
2020-01-27 22:40:35 -0600

    Test

--- index.js
+++ index.js
@@ -6,15 +6,8 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
   }
 
 app.post('/api/form', (req, res) => {
