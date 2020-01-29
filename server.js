917c51260b31043bd74a492685700cc355527c9e
James Robbins <jameslrobbins86@gmail.com>
2020-01-28 18:44:11 -0600

    Heroku Fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 app.post('/api/form', (req, res) => {
